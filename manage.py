68507531dfdf508f803a0b57eb5206266fb643e4
khauhelo <khauhelo872@gmail.com>
2025-08-05 00:05:28 -0400

    fix 1

--- manage.py
+++ manage.py
@@ -1,8 +1,7 @@
 import os
 from application import create_app, db
 from flask_migrate import Migrate  # type: ignore
-from gunicorn.app.wsgiapp import WSGIApplication
-import sys
+
 
 # Create the Flask app instance
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
@@ -11,6 +10,4 @@ migrate = Migrate(app, db)
 # Optional: Define the shell context
 def make_shell_context():
     return dict(app=app, db=db)
-
-if __name__ == '__main__':
-    # Added a condition to run the app only when needed
+app.shell_context_processor(make_shell_context)
