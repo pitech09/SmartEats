55bf69cc52f40205a0145341a9fe9ab77fab6006
khauhelo <khauhelo872@gmail.com>
2025-08-04 23:47:59 -0400

    change manage

--- manage.py
+++ manage.py
@@ -14,8 +14,3 @@ def make_shell_context():
 
 if __name__ == '__main__':
     # Added a condition to run the app only when needed
-    #if len(sys.argv) == 1:
-    
-     #   sys.argv += ['manage:app', '--bind=0.0.0.0:8000', '--workers=12', '--threads=4']
-    #sys.exit(WSGIApplication("%(prog)s [OPTIONS] [APP_MODULE]").run())
-    #app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8000)))
