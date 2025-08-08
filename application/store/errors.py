001dd313a702fd2c55082d6bbbaca1806f4d50b7
khauhelo <khauhelo872@gmail.com>
2025-08-08 01:23:28 -0400

    cool

--- application/store/errors.py
+++ application/store/errors.py
@@ -1,22 +1,22 @@
 from flask import render_template
 from ..forms import Set_StoreForm
 
-from . import main
+from . import store
 
 
-@main.app_errorhandler(404)
+@store.app_errorhandler(404)
 def page_not_found(e):
     formpharm=Set_StoreForm()
     return render_template('store/404.html', formpharm=formpharm), 404
 
 
-@main.app_errorhandler(401)
+@store.app_errorhandler(401)
 def unauthorized(error):
     formpharm=Set_StoreForm()
     return render_template('store/401.html', formpharm=formpharm), 401
 
 
-@main.app_errorhandler(500)
+@store.app_errorhandler(500)
 def internal_server_error(e):
     formpharm=Set_StoreForm()
     return render_template('store/500.html', formpharm=formpharm), 500
