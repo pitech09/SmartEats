0437452fe1d062897809602c2fa1a076d4b22020
khauhelo <khauhelo872@gmail.com>
2025-08-08 00:59:04 -0400

    works help retirving

--- application/templates/store/errors.py
+++ application/templates/store/errors.py
@@ -7,16 +7,16 @@ from . import main
 @main.app_errorhandler(404)
 def page_not_found(e):
     formpharm=Set_StoreForm()
-    return render_template('store/404.html', formpharm=formpharm), 404
+    return render_template('404.html', formpharm=formpharm), 404
 
 
 @main.app_errorhandler(401)
 def unauthorized(error):
     formpharm=Set_StoreForm()
-    return render_template('store/401.html', formpharm=formpharm), 401
+    return render_template('401.html', formpharm=formpharm), 401
 
 
 @main.app_errorhandler(500)
 def internal_server_error(e):
     formpharm=Set_StoreForm()
-    return render_template('store/500.html', formpharm=formpharm), 500
+    return render_template('500.html', formpharm=formpharm), 500
