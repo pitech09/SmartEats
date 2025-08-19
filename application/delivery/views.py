57c085a12979ae737c65812a1c7d45528696a214
khauhelo <khauhelo872@gmail.com>
2025-08-19 10:49:55 -0400

    changes

--- application/delivery/views.py
+++ application/delivery/views.py
@@ -45,6 +45,39 @@ def upload_to_cloudinary(file, folder='delivery_proofs'):
     return result
 
 
+def save_product_picture(file):
+    # Set the desired size for resizing
+    size = (300, 300)
+
+    # Generate a random hex string for the filename
+    random_hex = secrets.token_hex(9)
+
+    # Get the file extension
+    _, f_ex = os.path.splitext(file.filename)
+
+    # Generate the final filename (random + extension)
+    post_img_fn = random_hex + f_ex
+
+    # Define the path to save the file (UPLOAD_PRODUCTS should be configured in your Flask app)
+    post_image_path = os.path.join(current_app.root_path, current_app.config['UPLOAD_PRODUCTS'], post_img_fn)
+
+    try:
+        # Open the image
+        img = Image.open(file)
+
+        # Resize the image to fit within the size (thumbnail)
+        img.thumbnail(size)
+
+        # Save the resized image
+        img.save(post_image_path)
+
+        return post_img_fn  # Return the filename to store in the database
+    except Exception as e:
+        # If an error occurs during image processing, handle it
+        print(f"Error saving image: {e}")
+        return None
+    
+
 @delivery.route('/dashboard', methods=["GET", "POST"])
 @login_required
 def dashboard():
@@ -186,10 +219,15 @@ def update_delivery(delivery_id):
         order.status = form.status.data
         delivery.end_time = datetime.utcnow()
         if form.delivery_prove.data:
-            image_filename = upload_to_cloudinary(form.delivery_prove.data)
-            image_url = image_filename['secure_url'] 
-            delivery.customer_pic = image_url
-        db.session.add(delivery)
+            if current_app.config['USE_CLOUDINARY']:
+                image_filename = upload_to_cloudinary(form.delivery_prove.data)
+                image_url = image_filename['secure_url'] 
+                delivery.customer_pic = image_url
+                db.session.add(delivery)
+            else:
+                pic = save_product_picture(form.delivery_prove.data)
+                delivery.customer_pic = pic
+                db.session.add(delivery)
         db.session.add(order)
         try:
             db.session.commit()
