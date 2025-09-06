eba5bdb87c1e74f4c7ec6d81f5f11fb0d921fd8a
khauhelo <khauhelo872@gmail.com>
2025-09-06 11:09:51 -0400

    ahaaaaa

--- config.py
+++ config.py
@@ -75,10 +75,11 @@ class ProductionConfig(Config):
     USE_CLOUDINARY = True
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or 'pitechcorp7@gmail.com'
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or 'rljm azij wply ihrp'
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        "DATABASE_URL",
-        "postgresql+psycopg2://postgres:Boity%202003@db.lqqhcvfotzowyitgnfgf.supabase.co:5432/postgres"
-    )
+    SQLALCHEMY_DATABASE_URI = (
+    "postgresql://postgres.lqqhcvfotzowyitgnfgf:Boity%202003"
+    "@aws-1-sa-east-1.pooler.supabase.com:5432/postgres?sslmode=require"
+)
+
     # Configuration
     cloudinary.config(
         cloud_name = 'di9fnjxk5',
