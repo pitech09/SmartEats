8490817519ac4acb9eb0b8d1509755afd65ccf62
khauhelo <khauhelo872@gmail.com>
2025-11-07 06:32:23 -0500

    updated email

--- application/email.py
+++ application/email.py
@@ -12,6 +12,6 @@ def send_async_email(app, msg):
 def send_email(to, subject, template, **kwargs):
     msg = Message(current_app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject,
     sender=current_app.config['FLASKY_MAIL_SENDER'], recipients=[to])
-    msg.body = render_template('templates/auth/email/confirm' + '.txt', **kwargs)
+    msg.body = render_template('templates/auth/email/confirm' + '.html', **kwargs)
     #msg.html = render_template(template + '.html', **kwargs)
     mail.send(msg)
