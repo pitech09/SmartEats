cbf1b5364f8dc9dd350cf60a2d600d4ce41766f6
khauhelo <khauhelo872@gmail.com>
2025-12-10 23:37:12 -0500

    not yet

--- config.py
+++ config.py
@@ -102,5 +102,5 @@ class ProductionConfig(Config):
 config = {
     'development': DevelopmentConfig,
     'production': ProductionConfig,
-    'default': DevelopmentConfig
+    'default': ProductionConfig
 }
