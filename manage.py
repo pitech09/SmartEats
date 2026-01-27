3b917f8486d11b858d9e712a64d1629635902501
khauhelo <khauhelo872@gmail.com>
2026-01-27 12:53:12 -0500

    refactor: Update development server configuration to use socketio for running the app

--- manage.py
+++ manage.py
@@ -23,11 +23,11 @@ def make_shell_context():
 
 # Development only
 if __name__ == "__main__":
-    app.run('0.0.0.0', port=5000, debug=True)
-  #  socketio.run(
-    #    app,
-   #     host="0.0.0.0",
-  #      port=5000,
- #       debug=True,
- #       use_reloader=False
-#    )
+    #app.run('0.0.0.0', port=5000, debug=True)
+    socketio.run(
+       app,
+        host="0.0.0.0",
+       port=5000,
+        debug=True,
+        use_reloader=False
+    )
